22b24edd9ded776d2801041a379da1a6855e6946
GreyBalloonYU <cvdfed_19981009@126.com>
2018-09-13 22:28:31 +0800

    9月13日

--- dazhongsi/src/arrange.js
+++ dazhongsi/src/arrange.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
+import {Row,Col,Button,List} from 'antd';
 
 class Arrange extends React.Component{
     render(){
+        const data=[
+            '2018.9.10 17 : 00 - 19 : 30',
+            '2018.9.12 17 : 00 - 19 : 30',
+            '2018.9.15 13 : 00 - 15 : 00',
+        ];
         return(
             <div>
-            <br/><br/><br/><br/><br/><br/>
-            <div>Arrange</div>
+               <Row>
+                 <Col xs={24} sm={{span:12,offset:6}}>
+                   <div style={{fontSize:"16px",marginTop:"30px",position:"relative"}}>
+                   我的排班表
+                   <List
+                      bordered
+                      dataSource={data}
+                      renderItem={item => (<List.Item>{item}</List.Item>)}
+                      size="large"
+                    />
+                   </div>
+                 </Col>
+               </Row>
+               <br/>
+               <Row>
+                 <Col xs={24} sm={{span:12,offset:6}}>
+                   <Button type="primary">查询未来一周的排班</Button>
+                 </Col>
+               </Row> 
             </div>
         )
     }
--- dazhongsi/src/checkin.js
+++ dazhongsi/src/checkin.js
@@ -1,13 +1,117 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
+import {Row,Col,Button,Tag} from 'antd';
+
+class CheckInTag extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            content:this.props.isCheckIn?"已签到":"未签到",
+            color:this.props.isCheckIn?"green":"red",
+        }
+    }
+
+    handleCheckin=()=>{
+        this.setState({content:"已签到",color:"green"});
+    }
+
+    render(){
+        return(
+            <div>
+             <span style={{ marginLeft:"20px",marginRight:"20px",borderStyle:"solid",borderWidth:"thin",paddingLeft:30,paddingRight:30,borderColor:"#AAAAAA"}}>
+             {this.props.text}
+             </span>
+             <Tag color={this.state.color} style={{marginRight:"20px"}}>{this.state.content}</Tag>
+             <Button onClick={this.handleCheckin} disabled={this.state.content=="未签到"?false:true}>签到</Button>
+            </div>
+        )
+    }
+}
 
 class Checkin extends React.Component{
     render(){
         return(
             <div>
-
-            <div>Check In</div>
+              <Row>
+                <Col xs={24} sm={{span:6,offset:6}}>
+                   <div style={{fontSize:"16px",marginTop:"30px",position:"relative"}}>
+                   用户名
+                   <span style={{ marginLeft:"20px",borderStyle:"solid",borderWidth:"thin",paddingLeft:30,paddingRight:30,borderColor:"#AAAAAA"}}>
+                   null
+                   </span>
+                   </div>
+                </Col>
+                <Col xs={24} sm={6}>
+                   <div style={{fontSize:"16px",marginTop:"30px",position:"relative"}}>
+                   身份证号
+                   <span style={{ marginLeft:"20px",borderStyle:"solid",borderWidth:"thin",paddingLeft:30,paddingRight:30,borderColor:"#AAAAAA"}}>
+                   null
+                   </span>
+                   </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={24} sm={{span:6,offset:6}}>
+                   <div style={{fontSize:"16px",marginTop:"30px",position:"relative"}}>
+                   姓名
+                   <span style={{ marginLeft:"20px",borderStyle:"solid",borderWidth:"thin",paddingLeft:30,paddingRight:30,borderColor:"#AAAAAA"}}>
+                   null
+                   </span>
+                   </div>
+                </Col>
+                <Col xs={24} sm={6}>
+                   <div style={{fontSize:"16px",marginTop:"30px",position:"relative"}}>
+                   电话
+                   <span style={{ marginLeft:"20px",borderStyle:"solid",borderWidth:"thin",paddingLeft:30,paddingRight:30,borderColor:"#AAAAAA"}}>
+                   null
+                   </span>
+                   </div>
+                </Col>
+                <Col xs={24} sm={6}>
+                  <Button type="primary">修改用户信息</Button>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={24} sm={{span:6,offset:6}}>
+                   <div style={{fontSize:"16px",marginTop:"30px",position:"relative"}}>
+                   年龄
+                   <span style={{ marginLeft:"20px",borderStyle:"solid",borderWidth:"thin",paddingLeft:30,paddingRight:30,borderColor:"#AAAAAA"}}>
+                   null
+                   </span>
+                   </div>
+                </Col>
+                <Col xs={24} sm={6}>
+                   <div style={{fontSize:"16px",marginTop:"30px",position:"relative"}}>
+                   地址
+                   <span style={{ marginLeft:"20px",borderStyle:"solid",borderWidth:"thin",paddingLeft:30,paddingRight:30,borderColor:"#AAAAAA"}}>
+                   null
+                   </span>
+                   </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={24} sm={{span:12,offset:6}}>
+                   <div style={{fontSize:"16px",marginTop:"30px",position:"relative"}}>
+                   备注
+                   <span style={{ marginLeft:"20px",borderStyle:"solid",borderWidth:"thin",paddingLeft:30,paddingRight:30,borderColor:"#AAAAAA"}}>
+                   null
+                   </span>
+                   </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={24} sm={{span:12,offset:6}}>
+                   <div style={{fontSize:"16px",marginTop:"30px",position:"relative"}}>
+                   今日签到
+                   <CheckInTag text="今天14 : 30至16 : 30" isCheckIn={1}/>
+                   <CheckInTag text="今天13 : 30至14 : 30" isCheckIn={0}/>
+                   <CheckInTag text="今天7 : 30至9 : 30" isCheckIn={1}/>
+                   <CheckInTag text="今天6 : 30至7 : 30" isCheckIn={0}/>
+                   </div>
+                </Col>
+              </Row>
+              <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
             </div>
         )
     }
